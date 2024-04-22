01b1bf5e9a1f97f2e4c1dfc3723be23dba43d70a
cyh <1909625265@qq.com>
2024-04-23 00:06:26 +0800

    update setup-tools.sh

---
+++ setup-tools.sh
@@ -0,0 +1,3 @@
+apt update
+apt install proxychains4 shadowsocks-libev vim wget git tmux make gcc time curl libreadline6-dev libsdl2-dev gcc-riscv64-linux-gnu openjdk-11-jre zlib1g-dev device-tree-compiler flex autoconf bison sqlite3 libsqlite3-dev zstd libzstd-dev
+sh -c "curl -L https://github.com/com-lihaoyi/mill/releases/download/0.11.6/0.11.6 > /usr/local/bin/mill && chmod +x /usr/local/bin/mill"
